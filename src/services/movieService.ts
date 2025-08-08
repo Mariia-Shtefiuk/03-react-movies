64ad60a3299cbcb73389a8b2198bf883a18b264a
Mariia Shtefiuk <masha.shtf@gmail.com>
2025-08-08 23:31:35 +0300

    create all components files

---
+++ src/services/movieService.ts
@@ -0,0 +1,27 @@
+import axios from "axios";
+import { Movie } from "../types/movies";
+
+const API_KEY = import.meta.env.VITE_TMDB_TOKEN;
+const BASE_URL = "https://api.themoviedb.org/3";
+
+interface FetchMoviesResponse {
+  results: Movie[];
+}
+
+export async function fetchMovies(query: string): Promise<Movie[]> {
+  const response = await axios.get<FetchMoviesResponse>(
+    `${BASE_URL}/search/movie`,
+    {
+      params: {
+        api_key: API_KEY,
+        query,
+        language: "en-US",
+        include_adult: false,
+      },
+      headers: {
+        Authorization: `Bearer ${API_KEY}`,
+      },
+    }
+  );
+  return response.data.results;
+}
